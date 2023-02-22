571d4d7b53b0e861789093c047f6ee32e3bf427d
Kelvin <kelvinyrb@gmail.com>
2023-02-22 11:07:25 +0000

    Upload .env for airflow and update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,23 +4,11 @@ provider "google" {
   zone    = var.zone
 }
 
-# resource "google_compute_firewall" "firewall" {
-#   name    = "firewall-externalssh"
-#   network = "default"
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["22"]
-#   }
-#   source_ranges = ["0.0.0.0/0"] # Not So Secure. Limit the Source Range
-#   target_tags   = ["externalssh"]
-# }
-
 # We create a public IP address for our google compute instance to utilize
 resource "google_compute_address" "static" {
   name = "vm-public-address"
   project = var.project
   region = var.region
-  # depends_on = [ google_compute_firewall.firewall ]
 }
 
 # Google Compute VM Instance
@@ -46,17 +34,13 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   provisioner "file" {
-   # source file name on the local machine where you execute terraform plan and apply
    source      = "~/.gc/"
-   # destination is the file location on the newly created instance
    destination = ".gc/"
    connection {
      host        = google_compute_address.static.address
      type        = "ssh"
-     # username of the instance would vary for each account refer the OS Login in GCP documentation
      user        = var.user 
      timeout     = "500s"
-     # private_key being used to connect to the VM. ( the public key was copied earlier using metadata )
      private_key = file(var.privatekeypath)
    }
   }
@@ -70,14 +54,13 @@ resource "google_compute_instance" "vm_instance" {
       private_key = file(var.privatekeypath)
     }
     inline = [
-      "wget https://raw.githubusercontent.com/kelvinyrb/de-project/main/terraform/install-dependencies.sh",
-      "chmod +x install-dependencies.sh",
-      "./install-dependencies.sh",
-      
+      "git clone https://github.com/kelvinyrb/de-project.git",
+      "chmod +x de-project/terraform/install-dependencies.sh",
+      "./de-project/terraform/install-dependencies.sh",
+      "cd de-project/airflow",
+      "docker-compose build"
     ]
   }
-
-  # depends_on = [ google_compute_firewall.firewall ]
   
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
@@ -89,42 +72,36 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
-# # VPC Network
-# resource "google_compute_network" "vpc_network" {
-#   name                    = "terraform-network"
-#   auto_create_subnetworks = "true"
-# }
+# Data Lake Bucket
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+resource "google_storage_bucket" "gcs_bucket" {
+  name          = "${var.bucket}_${var.project}" # Concatenating DL bucket & Project name for unique naming
+  location      = var.region
 
-# # Data Lake Bucket
-# # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
-# resource "google_storage_bucket" "gcs_bucket" {
-#   name          = "${var.bucket}_${var.project}" # Concatenating DL bucket & Project name for unique naming
-#   location      = var.region
+  # Optional, but recommended settings:
+  storage_class = var.storage_class
+  uniform_bucket_level_access = true
 
-#   # Optional, but recommended settings:
-#   storage_class = var.storage_class
-#   uniform_bucket_level_access = true
+  versioning {
+    enabled     = true
+  }
 
-#   versioning {
-#     enabled     = true
-#   }
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
 
-#   lifecycle_rule {
-#     action {
-#       type = "Delete"
-#     }
-#     condition {
-#       age = 30  // days
-#     }
-#   }
+  force_destroy = true
+}
 
-#   force_destroy = true
-# }
-
-# # Data Warehouse
-# # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
-# resource "google_bigquery_dataset" "dataset" {
-#   dataset_id = var.bq_dataset
-#   project    = var.project
-#   location   = var.region
-# }
+# Data Warehouse
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = var.bq_dataset
+  project    = var.project
+  location   = var.region
+}
