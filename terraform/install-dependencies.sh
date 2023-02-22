c7e3bff8295f007df6f13f6fe2238a20c99c70e4
Kelvin <kelvinyrb@gmail.com>
2023-02-22 09:23:40 +0000

    Update install-depenpendies script

--- terraform/install-dependencies.sh
+++ terraform/install-dependencies.sh
@@ -11,17 +11,15 @@ else
     source "$bashrc_path"
 fi
 
-# Install Anaconda on VM
-wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
-bash Anaconda3-2022.10-Linux-x86_64.sh
-rm https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+# Authenticate service account
+echo "Key: $GOOGLE_APPLICATION_CREDENTIALS"
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 
 # Install docker
 sudo apt-get update
-sudo apt-get install docker.io
+sudo apt-get install -y docker.io
 
 # Set up docker without sudo
-https://github.com/sindresorhus/guides/blob/main/docker-without-sudo.md
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 sudo service docker restart
@@ -32,11 +30,8 @@ cd bin
 wget https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -O docker-compose
 chmod +x docker-compose
 
-# Install Google SDK
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-418.0.0-linux-x86_64.tar.gz
-tar -xf google-cloud-cli-418.0.0-linux-x86_64.tar.gz
-rm google-cloud-cli-418.0.0-linux-x86_64.tar.gz
-./google-cloud-sdk/install.sh
-
-# Authenticate service account
-gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
+# # Install Google SDK
+# curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-418.0.0-linux-x86_64.tar.gz
+# tar -xf google-cloud-cli-418.0.0-linux-x86_64.tar.gz
+# rm google-cloud-cli-418.0.0-linux-x86_64.tar.gz
+# yes | ./google-cloud-sdk/install.sh
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,23 +4,23 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_compute_firewall" "firewall" {
-  name    = "firewall-externalssh"
-  network = "default"
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-  source_ranges = ["0.0.0.0/0"] # Not So Secure. Limit the Source Range
-  target_tags   = ["externalssh"]
-}
+# resource "google_compute_firewall" "firewall" {
+#   name    = "firewall-externalssh"
+#   network = "default"
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["22"]
+#   }
+#   source_ranges = ["0.0.0.0/0"] # Not So Secure. Limit the Source Range
+#   target_tags   = ["externalssh"]
+# }
 
 # We create a public IP address for our google compute instance to utilize
 resource "google_compute_address" "static" {
   name = "vm-public-address"
   project = var.project
   region = var.region
-  depends_on = [ google_compute_firewall.firewall ]
+  # depends_on = [ google_compute_firewall.firewall ]
 }
 
 # Google Compute VM Instance
@@ -28,7 +28,7 @@ resource "google_compute_address" "static" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-standard-2"
-  tags         = ["externalssh"]
+  # tags         = ["externalssh"]
 
   boot_disk {
     initialize_params {
@@ -70,12 +70,14 @@ resource "google_compute_instance" "vm_instance" {
       private_key = file(var.privatekeypath)
     }
     inline = [
-      "credentials_path=\".gc/gc-key.json\"",
-      "bash install-dependencies.sh"
+      "wget https://raw.githubusercontent.com/kelvinyrb/de-project/main/terraform/install-dependencies.sh",
+      "chmod +x install-dependencies.sh",
+      "./install-dependencies.sh",
+      
     ]
   }
 
-  depends_on = [ google_compute_firewall.firewall ]
+  # depends_on = [ google_compute_firewall.firewall ]
   
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
@@ -87,42 +89,42 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
-# VPC Network
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
-}
+# # VPC Network
+# resource "google_compute_network" "vpc_network" {
+#   name                    = "terraform-network"
+#   auto_create_subnetworks = "true"
+# }
 
-# Data Lake Bucket
-# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
-resource "google_storage_bucket" "gcs_bucket" {
-  name          = "${var.bucket}_${var.project}" # Concatenating DL bucket & Project name for unique naming
-  location      = var.region
+# # Data Lake Bucket
+# # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+# resource "google_storage_bucket" "gcs_bucket" {
+#   name          = "${var.bucket}_${var.project}" # Concatenating DL bucket & Project name for unique naming
+#   location      = var.region
 
-  # Optional, but recommended settings:
-  storage_class = var.storage_class
-  uniform_bucket_level_access = true
+#   # Optional, but recommended settings:
+#   storage_class = var.storage_class
+#   uniform_bucket_level_access = true
 
-  versioning {
-    enabled     = true
-  }
+#   versioning {
+#     enabled     = true
+#   }
 
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-    condition {
-      age = 30  // days
-    }
-  }
+#   lifecycle_rule {
+#     action {
+#       type = "Delete"
+#     }
+#     condition {
+#       age = 30  // days
+#     }
+#   }
 
-  force_destroy = true
-}
+#   force_destroy = true
+# }
 
-# Data Warehouse
-# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = var.bq_dataset
-  project    = var.project
-  location   = var.region
-}
+# # Data Warehouse
+# # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+# resource "google_bigquery_dataset" "dataset" {
+#   dataset_id = var.bq_dataset
+#   project    = var.project
+#   location   = var.region
+# }
